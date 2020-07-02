61a2473725602d9212920b452cdaadfc9b177221
FernandoHernandezVazquez <1719210003@utectulancingo.edu.mx>
2020-07-02 17:13:08 +0000

    acciones completas

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import web
 
 urls = (
     '/', 'application.controllers.fields.Formulario',
+    '/delete', 'application.controllers.delete.Delete',
+    '/insert', 'application.controllers.insert.Insert',
+    '/list', 'application.controllers.list.List',
+    '/update', 'application.controllers.update.Update',
+    '/view', 'application.controllers.view.View',
 )
 app = web.application(urls, globals())
 

7bb05d447cff0127455c4dbb2fb3b469f734bdda
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2020-07-18 15:45:47 -0500

    Add files via upload

---
+++ sql/alumnos.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE alumnos;
+
+USE alumnos;
+
+CREATE TABLE students(
+    id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre varchar(100) NOT NULL,
+    pri_ap varchar(100) NOT NULL,
+    seg_ap varchar(100) NOT NULL,
+    age int(10) NOT NULL,
+    date date NOT NULL,
+    gender varchar(20) NOT NULL,
+    state varchar(100) NOT NULL
+) ENGINE= InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO personas(nombre,email)
+VALUES
+('Fernando','Hernandez', 'Vazquez', '20','27-01-00', 'masculino','hidalgo');
+
+CREATE USER 'user_alumno'@'localhost' IDENTIFIED BY 'sphinx';
+GRANT ALL PRIVILEGES ON agenda_db.* TO 'user_alumno'@'localhost';
+FLUSH PRIVILEGES;

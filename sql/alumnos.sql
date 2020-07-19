1fd030e71777ea5424ea3f8626f18141a6b7bfeb
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2020-07-19 11:14:21 -0500

    Add files via upload

--- sql/alumnos.sql
+++ sql/alumnos.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS alumnos;
 CREATE DATABASE alumnos;
 
 USE alumnos;
@@ -8,15 +9,19 @@ CREATE TABLE students(
     pri_ap varchar(100) NOT NULL,
     seg_ap varchar(100) NOT NULL,
     age int(10) NOT NULL,
-    date date NOT NULL,
+    fdate varchar(10) not null ,
     gender varchar(20) NOT NULL,
     state varchar(100) NOT NULL
 ) ENGINE= InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO personas(nombre,pri_ap,seg_ap,age,date,gender,state)
+INSERT INTO students(id,nombre,pri_ap,seg_ap,age,fdate,gender,state)
 VALUES
-('Fernando','Hernandez', 'Vazquez', '20','27-01-00', 'masculino','hidalgo');
+('1718110040','Fernando','Hernandez', 'Vazquez', '20','2000/01/27', 'Masculino','hidalgo'),
+('1719112255','Guadalupe','Espinoz','Riveros','20','2000/05/04','Femenino','Hidalgo');
 
-CREATE USER 'user_alumno'@'localhost' IDENTIFIED BY 'sphinx';
+/*CREATE USER 'alumno'@'localhost' IDENTIFIED BY 'sphinx';
 GRANT ALL PRIVILEGES ON agenda_db.* TO 'user_alumno'@'localhost';
-FLUSH PRIVILEGES;
+FLUSH PRIVILEGES;*/
+
+
+

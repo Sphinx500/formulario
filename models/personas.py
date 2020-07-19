75299a854e3d9571efc2597f027d4c1d1e8707b2
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2020-07-19 11:14:48 -0500

    Add files via upload

--- models/personas.py
+++ models/personas.py
@@ -8,7 +8,7 @@ class Personas():
         try:
             self.cnx = mysql.connector.connect(
                 user='root',
-                password='sphinx',
+                password='Sphonx',
                 host='127.0.0.1',
                 port=3306,
                 database='alumnos'
@@ -19,19 +19,19 @@ class Personas():
     def select(self):
         try:
             self.connect()
-            query = ("SELECT * FROM personass;")
+            query = ("SELECT * FROM students;")
             self.cursor.execute(query)
             result = []
             for row in self.cursor:
                 r = {
-                    'id':row[1],
-                    'nombre':row[2],
-                    'pri_ap':row[3],
-                    'seg_ap':row[4],
-                    'age':row[5],
-                    'date':row[6],
-                    'gender':row[7],
-                    'state':row[8]
+                    'id':row[0],
+                    'nombre':row[1],
+                    'pri_ap':row[2],
+                    'seg_ap':row[3],
+                    'age':row[4],
+                    'date':row[5],
+                    'gender':row[6],
+                    'state':row[7]
                 }
                 result.append(r)
             self.cursor.close()

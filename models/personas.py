8c27e5c32833968859f12069cadfa6032243c222
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2020-08-02 14:19:10 -0500

    Add files via upload

--- models/personas.py
+++ models/personas.py
@@ -1,14 +1,12 @@
 
 import mysql.connector
-
-
 class Personas():
 
     def connect(self):
         try:
             self.cnx = mysql.connector.connect(
                 user='root',
-                password='Sphonx',
+                password='1234',
                 host='127.0.0.1',
                 port=3306,
                 database='alumnos'
@@ -24,14 +22,14 @@ class Personas():
             result = []
             for row in self.cursor:
                 r = {
-                    'id':row[0],
-                    'nombre':row[1],
-                    'pri_ap':row[2],
-                    'seg_ap':row[3],
-                    'age':row[4],
-                    'date':row[5],
-                    'gender':row[6],
-                    'state':row[7]
+                    'id':row[1],
+                    'nombre':row[2],
+                    'pri_ap':row[3],
+                    'seg_ap':row[4],
+                    'age':row[5],
+                    'date':row[6],
+                    'gender':row[7],
+                    'state':row[8]
                 }
                 result.append(r)
             self.cursor.close()
@@ -41,6 +39,45 @@ class Personas():
             print(e)
             result = []
             return result
+
+    def view(self,id):
+        try:
+            self.connect()
+            query = ("SELECT * FROM students where id = %s;")
+            values = (id,)
+            self.cursor.execute(query,values)
+            result = []
+            for row in self.cursor:
+                r = {
+                    'id':row[1],
+                    'nombre':row[2],
+                    'pri_ap':row[3],
+                    'seg_ap':row[4],
+                    'age':row[5],
+                    'date':row[6],
+                    'gender':row[7],
+                    'state':row[8]
+                }
+                result.append(r)
+            self.cursor.close()
+            self.cnx.close()
+            return result
+        except Exception as e:
+            print(e)
+
+    def insert(self, nombre, email):
+        try:
+            self.connect()
+            query = ("INSERT INTO students (nombre, pri_ap, seg_ap, age, date, gender, state) values(%s,%s,%s,%s,%s,%s,%s);")
+            values = (nombre, pri_ap, seg_ap, age, date, gender, state)
+            self.cursor.execute(query, values)
+            self.cnx.commit()
+            self.cursor.close()
+            self.cnx.close()
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
 objeto = Personas()
 objeto.connect() 

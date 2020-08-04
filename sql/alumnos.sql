ceddb086d7feebf985da0e47b450cb2b295bf8b8
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2020-08-04 09:19:46 -0500

    Add files via upload

--- sql/alumnos.sql
+++ sql/alumnos.sql
@@ -4,17 +4,17 @@ CREATE DATABASE alumnos;
 USE alumnos;
 
 CREATE TABLE students(
-    id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre varchar(100) NOT NULL,
+    matricula int(11) NOT NULL  PRIMARY KEY,
+    nombre char(10) NOT NULL,
     pri_ap varchar(100) NOT NULL,
     seg_ap varchar(100) NOT NULL,
-    age int(10) NOT NULL,
-    fdate varchar(10) not null ,
+    age char(3) NOT NULL,
+    fdate date ,
     gender varchar(20) NOT NULL,
     state varchar(100) NOT NULL
 ) ENGINE= InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO students(id,nombre,pri_ap,seg_ap,age,fdate,gender,state)
+INSERT INTO students(matricula,nombre,pri_ap,seg_ap,age,fdate,gender,state)
 VALUES
 ('1718110040','Fernando','Hernandez', 'Vazquez', '20','2000/01/27', 'Masculino','hidalgo'),
 ('1719112255','Guadalupe','Espinoz','Riveros','20','2000/05/04','Femenino','Hidalgo');

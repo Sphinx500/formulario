f44bbf78853b271345a100b7ac14ea807fa00266
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2020-08-09 16:40:19 -0500

    Add files via upload

--- application/controllers/personas/update.py
+++ application/controllers/personas/update.py
@@ -1,12 +1,32 @@
 import web 
 import app 
 
+import application.models.personas as personas
+model_personas = personas.Personas()
 render=web.template.render('application/views/personas/')
 
 class Update():
 
-    def GET(self):
+    def GET(self, matricula):
       try:
-        return render.update()
+        result = model_personas.view(matricula)[0]
+        #print(result)
+        return render.update(result)
       except Exception as e:
         return "Error" + str(e.args)
+    def POST(self, matricula):
+        try:
+            form = web.input()
+            matricula = form.matricula
+            nombre = form.nombre
+            pri_ap = form.pri_ap
+            seg_ap = form.seg_ap
+            age = form.age
+            fdate = form.fdate
+            gender = form.gender
+            state = form.state
+            model_personas.insert(matricula,nombre,pri_ap,seg_ap,age,fdate,gender,state)
+            web.seeother('/list')
+        except Exception as e:
+            print(e)
+            return "Error"
